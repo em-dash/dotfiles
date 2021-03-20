9c3d12db1f65a0830ef647100147534a4d984207
em-dash <33614480+em-dash@users.noreply.github.com>
2021-03-21 01:13:20 +1100

    rofi & alacritty: bespin theme

--- config/alacritty/set_theme.sh
+++ config/alacritty/set_theme.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# TODO make a version of this that reads a comment and chops off the colors
+# section of alacritty.yml and adds the chosen one back on
+
 if [ -f $1 ]; then
 	cp $(dirname $0)/alacritty.yml.withoutcolors $(dirname $0)/alacritty.yml
 	cat $1 >> alacritty.yml
