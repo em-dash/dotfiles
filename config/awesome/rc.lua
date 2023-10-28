2a48e2803c262b70795da96eb2947f82d68e992f
em-dash <33614480+em-dash@users.noreply.github.com>
2023-10-28 19:14:29 +1100

    aupddphkyursldausthuyahpdyuahpdulahpdyuaflhduyapdyuahllll

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -463,7 +463,7 @@ globalkeys = gears.table.join(
               {description = "restore minimized", group = "client"}),
 
     -- Prompt
-    awful.key({ modkey },        "space",     function () awful.screen.focused().mypromptbox:run() end,
+    awful.key({ modkey },        "r",     function () awful.screen.focused().mypromptbox:run() end,
               {description = "run prompt", group = "launcher"}),
 
     -- awful.key({ modkey }, "x",
