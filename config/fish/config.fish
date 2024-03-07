a287b3db48c641db2d27f3eaef853654e8d9a44c
em-dash <33614480+em-dash@users.noreply.github.com>
2024-03-07 20:10:18 +1100

    helix setup

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -11,8 +11,10 @@ alias lsl "ls -lAh | less"
 alias odx "od -t x1z"
 alias asdf "setxkbmap us colemak"
 alias quit "exit"
-alias qq "clang -Wall -std=c99 -pedantic"
 alias grep "grep --color=auto"
+if type -q helix
+    alias hx "helix"
+end
 
 set -g __fish_git_prompt_show_informative_status 1
 set -g __fish_git_prompt_use_informative_chars 1
