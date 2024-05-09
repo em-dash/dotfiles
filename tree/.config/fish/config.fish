544c2e7d3d632327a3c3818588b64f4f7a8a620a
em-dash <33614480+em-dash@users.noreply.github.com>
2024-05-09 11:46:29 +1000

    alias zellij edit

--- tree/.config/fish/config.fish
+++ tree/.config/fish/config.fish
@@ -10,12 +10,13 @@ alias ll "ls -lAh"
 alias lsl "ls -lAh | less"
 alias odx "od -t x1z"
 alias asdf "setxkbmap us colemak"
-alias quit "exit"
+alias quit exit
 alias grep "grep --color=auto"
-alias nvim "hx"
+alias nvim hx
+alias ze "zellij edit"
 
 if type -q helix
-    alias hx "helix"
+    alias hx helix
 end
 
 set -g __fish_git_prompt_show_informative_status 1
