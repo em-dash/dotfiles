f759d0e32a0aeedd6644258fcdec4fcf2db02d98
em-dash <33614480+em-dash@users.noreply.github.com>
2024-11-28 14:22:03 +1100

    ya

--- scripts/friendly.py
+++ scripts/friendly.py
@@ -1,10 +1,4 @@
 class __quit_workaround__:
-    _instance = None
     def __repr__(self): raise SystemExit
-    def __new__(cls):
-        if not __quit_workaround__._instance:
-            __quit_workaround__._instance = True
-            return super(__quit_workaround__, cls).__new__(cls)
-        else: return _instance
 
 exit = quit = __quit_workaround__()
